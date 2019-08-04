e7027b4c4a3712f911e8eefd89d0b32601c48d46
sourabhmadur <sourabhmadur@gmail.com>
2019-08-04 14:28:33 -0400

    modified json and index.js files

--- app/views/Index.js
+++ app/views/Index.js
@@ -8,11 +8,11 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
+          <h2><Link to="/">About this site yoyoyo</Link></h2>
           <p>A beautiful, responsive, react app written with modern Javascript.</p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Welcome to madur's website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
