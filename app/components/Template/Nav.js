763cca1097267f1dada478bc09986d2574ed96d9
sourabhmadur <sourabhmadur@gmail.com>
2019-08-04 14:36:20 -0400

    added my name in description

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -20,7 +20,7 @@ const Nav = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
+      <p>Hi, I&apos;m Sourabh. I like building things.
             I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
             the co-founder and CTO of <a href="http://arthena.com">Arthena</a>. Before Arthena I was
             at <a href="http://matroid.com">Matroid</a>, <a href="http://planet.com">Planet</a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.
