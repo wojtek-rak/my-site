a3aa877d0c9e985b72cc1d613bcddf871f2f6443
sourabhmadur <sourabhmadur@gmail.com>
2019-08-24 22:59:33 -0400

    contact and index changes

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -88,7 +88,7 @@ const Contact = () => {
           </div>
         </header>
         <div className="email-at">
-          <p>Feel free to get in touch. You can email me at: </p>
+          <p>Feel free to hit me up if you want to collaborate on projects, offering job oppurtunities or just want to meet up for coffee. You can email me at: </p>
           <div
             className="inline-container"
             style={validateText(message) ? {} : { color: 'red' }}
