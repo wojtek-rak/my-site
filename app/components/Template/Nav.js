85869b1cec5663ab851b38fe50bceb82098b54d9
wolix07 <rak.wojtek@wp.pl>
2019-10-07 12:47:57 +0200

    Update information and meta

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -13,17 +13,16 @@ const Nav = () => (
         <img src={`${BASE_PATH}/images/me_icon.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Wojciech Rak</h2>
+        <p><a href="mailto:rak.wojtek@wp.pl">rak.wojtek@wp.pl</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-            I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-            the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-            at <a href="https://matroid.com">Matroid</a>, <a href="https://planet.com">Planet</a>, <a href="https://planetaryresources.com">Planetary Resources</a>, <a href="https://facebook.com">Facebook</a>, and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Wojciech. I like building things.
+            I am student of IT at <a href="https://www.put.poznan.pl/en/">Poznan University of Technogy</a>, I'm currently working as junior .NET Developer at <a href="https://gfieast.com/en/">GFI East</a>. Before GFI East I was
+            at DomData.
       </p>
       <ul className="actions">
         <li>
