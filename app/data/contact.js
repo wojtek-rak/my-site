7745b7d40cde7bdb400c66ae5436bc1be7fcc77c
wolix07 <rak.wojtek@wp.pl>
2019-10-28 10:27:48 +0100

    Change media links

--- app/data/contact.js
+++ app/data/contact.js
@@ -2,43 +2,43 @@ import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
 import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
 import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
 import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
-import faAngellist from '@fortawesome/fontawesome-free-brands/faAngellist';
+import faStackOverflow from '@fortawesome/fontawesome-free-brands/faStackOverflow';
 import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
 import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
 
 const data = [
   {
-    link: 'https://github.com/mldangelo',
+    link: 'https://github.com/wojtek-rak',
     label: 'Github',
     icon: faGithub,
   },
   {
-    link: 'https://facebook.com/d',
+    link: 'https://www.facebook.com/rak.wojtek',
     label: 'Facebook',
     icon: faFacebook,
   },
   {
-    link: 'https://www.instagram.com/dangelosaurus/',
+    link: 'https://www.instagram.com/wojciech.r4k/',
     label: 'Instagram',
     icon: faInstagram,
   },
   {
-    link: 'https://www.linkedin.com/in/michaelldangelo',
+    link: 'https://www.linkedin.com/in/wojciechrak/',
     label: 'LinkedIn',
     icon: faLinkedinIn,
   },
   {
-    link: 'https://angel.co/michael-d-angelo',
+    link: 'https://stackoverflow.com/story/wolix',
     label: 'Angel List',
-    icon: faAngellist,
+    icon: faStackOverflow,
   },
   {
-    link: 'https://twitter.com/dangelosaurus',
+    link: 'https://twitter.com/WojciechR4k',
     label: 'Twitter',
     icon: faTwitter,
   },
   {
-    link: 'mailto:michael.l.dangelo@gmail.com',
+    link: 'mailto:wojciech.r4k@gmail.com',
     label: 'Email',
     icon: faEnvelope,
   },
