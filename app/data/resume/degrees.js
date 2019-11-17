d84cde38548023baf796dd00f062b19054a42595
wolix07 <rak.wojtek@wp.pl>
2019-11-17 15:00:07 +0100

    refactor resume

--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,15 +1,10 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  }, {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
+    school: 'Poznan University of Technology',
+    degree: 'currently',
+    link: 'https://www.put.poznan.pl/',
+    year: 2021,
+  }
 ];
 
 export default degrees;
