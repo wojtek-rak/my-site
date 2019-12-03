004c5afbde7f79e70cb2290c5a3d67344e3a4b20
Wojciech Rak <rak.wojtek@wp.pl>
2019-12-03 01:43:39 +0100

    Working gallery with images

--- app/data/events.js
+++ app/data/events.js
@@ -1,21 +1,29 @@
 const data = [
   {
-    title: 'Unit Tests - Introduction',
+    title: 'Unit Testing',
     link: 'http://localhost:7999/events',
     date: '2015-11-20',
     desc: 'Built for a social impact hackathon. Built for a social impact hackathon Built for a social impact hackathon ',
     images: [
       {
-        original: '/images/gallery/UNIT_TESTING/UNIT_TESTING_1-min.jpg',
-        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        original: 'images/gallery/UNIT_TESTING/UNIT_TESTING_1-min.png',
+        thumbnail: 'images/gallery/UNIT_TESTING//UNIT_TESTING_1_MINI-min.png',
       },
       {
-        original: 'https://picsum.photos/id/1015/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        original: 'images/gallery/UNIT_TESTING/UNIT_TESTING_2-min.jpg',
+        thumbnail: 'images/gallery/UNIT_TESTING//UNIT_TESTING_2_MINI-min.jpg',
       },
       {
-        original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        original: 'images/gallery/UNIT_TESTING/UNIT_TESTING_3-min.jpg',
+        thumbnail: 'images/gallery/UNIT_TESTING//UNIT_TESTING_3_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/UNIT_TESTING/UNIT_TESTING_4-min.jpg',
+        thumbnail: 'images/gallery/UNIT_TESTING//UNIT_TESTING_4_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/UNIT_TESTING/UNIT_TESTING_5-min.jpg',
+        thumbnail: 'images/gallery/UNIT_TESTING//UNIT_TESTING_5_MINI-min.jpg',
       },
     ]
   },
@@ -27,20 +35,67 @@ const data = [
     desc: 'Built for a social impact hackathon. ',
     images: [
       {
-        original: 'https://picsum.photos/id/1018/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        original: 'images/gallery/MVC/MVC_4-min.png',
+        thumbnail: 'images/gallery/MVC/MVC_4_MINI-min.png',
       },
       {
-        original: 'https://picsum.photos/id/1015/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        original: 'images/gallery/MVC/MVC_1-min.jpg',
+        thumbnail: 'images/gallery/MVC/MVC_1_MINI-min.jpg',
       },
       {
-        original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        original: 'images/gallery/MVC/MVC_2-min.jpg',
+        thumbnail: 'images/gallery/MVC/MVC_2_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/MVC/MVC_3-min.jpg',
+        thumbnail: 'images/gallery/MVC/MVC_3_MINI-min.jpg',
       },
     ]
   },
-
+  {
+    title: 'Testing in unity',
+    link: 'http://localhost:7999/events',
+    date: '2015-11-20',
+    desc: 'Built for a social impact hackathon. ',
+    images: [
+      {
+        original: 'images/gallery/UNITY_TESTING/UNITY_TESTING_1-min.jpg',
+        thumbnail: 'images/gallery/UNITY_TESTING/UNITY_TESTING_1_MINI-min.jpg',
+    },
+      {
+        original: 'images/gallery/UNITY_TESTING/UNITY_TESTING_2-min.jpg',
+        thumbnail: 'images/gallery/UNITY_TESTING/UNITY_TESTING_2_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/UNITY_TESTING/UNITY_TESTING_3-min.jpg',
+        thumbnail: 'images/gallery/UNITY_TESTING/UNITY_TESTING_3_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/UNITY_TESTING/UNITY_TESTING_4-min.jpg',
+        thumbnail: 'images/gallery/UNITY_TESTING/UNITY_TESTING_4_MINI-min.jpg',
+      },
+    ]
+  },
+  {
+    title: 'Game testing',
+    link: 'http://localhost:7999/events',
+    date: '2015-11-20',
+    desc: 'Built for a social impact hackathon. ',
+    images: [
+      {
+        original: 'images/gallery/GAMETESTING/GAMETESTING_1-min.jpg',
+        thumbnail: 'images/gallery/GAMETESTING/GAMETESTING_1_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/GAMETESTING/GAMETESTING_2-min.jpg',
+        thumbnail: 'images/gallery/GAMETESTING/GAMETESTING_2_MINI-min.jpg',
+      },
+      {
+        original: 'images/gallery/GAMETESTING/GAMETESTING_3-min.jpg',
+        thumbnail: 'images/gallery/GAMETESTING/GAMETESTING_3_MINI-min.jpg',
+      },
+    ]
+  },
 ];
 
 export default data;
--- app/views/Events.js
+++ app/views/Events.js
@@ -7,12 +7,6 @@ import Main from '../layouts/Main';
 import Cell from '../components/Events/Cell';
 import data from '../data/events';
 
-import ImageGallery from 'react-image-gallery';
-
-const images = [
-
-];
-
 const Events = () => (
     <Main>
         <Helmet title="Events" />
@@ -21,7 +15,6 @@ const Events = () => (
             <header>
                 <div className="title">
                     <h2><Link to="/events">Events</Link></h2>
-                    <img src="/images/gallery/UNIT_TESTING/UNIT_TESTING_1-min.jpg" />
                     <p>A selection of projects that I&apos;m not too ashamed of</p>
                 </div>
             </header>
