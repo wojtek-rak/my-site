0ca3359b0254c26b3429431ced79e2db0fc1a097
wolix07 <rak.wojtek@wp.pl>
2021-05-22 18:14:57 +0200

    Add ice master

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,6 +1,15 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Ice Master',
+    subtitle: '',
+    link: 'https://play.google.com/store/apps/details?id=com.gulij.icemaster',
+    image: 'https://scontent.fpoz3-1.fna.fbcdn.net/v/t1.6435-9/104853103_148534056867601_6270733954952448780_n.png?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=8H577qE-oFwAX8VH3TT&_nc_oc=AQmpPKm1sBDws-pNYxDzOy4YxfEhtVOQmRdVHyCbBFXv3TG581jtepoJ4y_7ZiE_jxM&_nc_ht=scontent.fpoz3-1.fna&oh=4427e5959d9f8d2422065a46d0f0c462&oe=60CEA681',
+    date: '2021-05-10',
+    desc: ' Dodge bats and collect gems. Watch out for the incoming thunderstorm!'
+      + 'How high can you climb? Check out your friends\' scores.',
+  },
+  {
     title: 'Paint Store',
     subtitle: '',
     link: 'https://github.com/wojtek-rak/PaintStore',
